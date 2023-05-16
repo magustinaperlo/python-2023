1d6519a215efee3597cdc9378d69d388109eda11
lucasDemaria60 <131877186+lucasDemaria60@users.noreply.github.com>
2023-05-16 15:02:01 -0300

    correccion de tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,4 +1,4 @@
-listadetuplas = [("termo", 600, 4, "bigstar"), ("vaso", 300, 8, "bigstar"), ("mate", 1200, 10, "posadas"), ("mate", 100, 20, "posadas"), ("termo", 30000, 2, "apple")]
+"""listadetuplas = [("termo", 600, 4, "bigstar"), ("vaso", 300, 8, "bigstar"), ("mate", 1200, 10, "posadas"), ("mate", 100, 20, "posadas"), ("termo", 30000, 2, "apple")]
 
 while (1):
     listaresultado = [ ]
@@ -12,35 +12,40 @@ while (1):
 
 
 
+meses=("enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre")
+num=int(input("ingrese un numero: "))
 
-tuplameses = ( "" , "enero" , "febrero" ,"marzo", "abril", "mayo" , "junio" , "julio" , "agosto" , "septiembre" , "octubre" , "noviembre" , "diciembre" )
-
-while 1:
-
-    num=int(input("ingrese un numero correspondiente a un mes:   "))
-
-    if num == 0:
-        os.system("cls")
-        op = input("\nprograma terminado. Precione cualquier tecla para continuar:  ")
-    elif (num < 13) and (num > 0):
-        for i in range(0,len(tuplameses)):
-            if i == num:
-                print (f"\n{tuplameses[i]}\n")
-    else:
-        print ("\nvalor ingresado no es correcto\n")
+if (num>=1 or num <13):
+    print(meses[num-1])
+if (num<1 or num>12):
+    print ("error")
 
 
+numeros=(1,2,1,2,1,2,1,1,2,1,1,2,)
+num=int(input("ingrese un numero: "))
+if (num == 1 or num == 2):
+    print(numeros.count(num))
+else:
+    print ("error")
+
+"""
+
+"""numeros2 = (12, 43, 12, 45, 2)
+
+mayor = numeros2[0]
+menor = numeros2[0]
+for i in numeros2:
+    if mayor < i:
+        mayor = i
+    if i < menor:
+        menor = i
+print ("El mayor es: " + str(mayor))
+print ("El menor es: " + str(menor))"""
 
 
-tuplanumeros = (1,1,4,4,4,2,3,3,3,3,5,5,6,7,7,7,7,8,8,9,9,9,9,9,10,11,12,12,12,12,12)
-
-while 1:
-    con = 0
-    try:
-        num = int(input("Ingrese un numero mayor a 0 y menor a 13: "))
-        if (num < 1) or (num > 12):
-            print("numero fuera de tupla")
-        else:
-            print(f"El numero aparece {tuplanumeros.count(num)} vez en la tupla")
-    except ValueError:
-        print ("Lo que ingresaste no era un numero: ") 
+numeros3=(1,2,3,4,5,6,7,8,9,10)
+indice=int(input("ingrese un indice "))
+if indice > 9 or indice < 0:
+    print("Indice inexistente")
+else:
+    print ("el valor de su indice es: " + str(numeros3[indice]))
